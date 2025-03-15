0bd138734cadee3f3c2157bea36155bb721d6b2b
smdsu <109036946+smdsu@users.noreply.github.com>
2025-03-15 17:44:04 +0300

    Add base change-password views

--- account/urls.py
+++ account/urls.py
@@ -4,12 +4,25 @@ from django.contrib.auth import views as auth_views
 
 from . import views
 
+
 urlpatterns = [
     # path('login/', views.user_login, name='login'),
 
     #Login/logout urls
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    #Change password urls
+    path(
+        'password-change/',
+        auth_views.PasswordChangeView.as_view(),
+        name='password_change'
+    ),
+    path(
+        'password-change/done/',
+        auth_views.PasswordChangeDoneView.as_view(),
+        name='password_change_done'
+    ),
 
     path('dashboard/', views.dashboard, name='dashboard'),
 ]
