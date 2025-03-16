f29ac01e8dbc84917448a80940bc5305b472382e
smdsu <109036946+smdsu@users.noreply.github.com>
2025-03-16 19:05:45 +0300

    Change auth urls to standart

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
+from django.urls import path, include
 
-from django.contrib.auth import views as auth_views
+# from django.contrib.auth import views as auth_views
 
 from . import views
 
@@ -9,43 +9,44 @@ urlpatterns = [
     # path('login/', views.user_login, name='login'),
 
     # Login/logout urls
-    path("login/", auth_views.LoginView.as_view(), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    # path("login/", auth_views.LoginView.as_view(), name="login"),
+    # path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 
     # Change password urls
-    path(
-        "password-change/",
-        auth_views.PasswordChangeView.as_view(),
-        name="password_change",
-    ),
-    path(
-        "password-change/done/",
-        auth_views.PasswordChangeDoneView.as_view(),
-        name="password_change_done",
-    ),
+    # path(
+    #    "password-change/",
+    #    auth_views.PasswordChangeView.as_view(),
+    #    name="password_change",
+    # ),
+    # path(
+    #     "password-change/done/",
+    #     auth_views.PasswordChangeDoneView.as_view(),
+    #     name="password_change_done",
+    # ),
 
-    # Reset password urls
-    path(
-        "password-reset/",
-        auth_views.PasswordResetView.as_view(),
-        name="password_reset",
-    ),
-    path(
-        "password-reset/done/",
-        auth_views.PasswordResetDoneView.as_view(),
-        name="password_reset_done",
-    ),
-    path(
-        "password-reset/<uidb64>/<token>/",
-        auth_views.PasswordResetConfirmView.as_view(),
-        name="password_reset_confirm",
-    ),
-    path(
-        "password-rest/complete/",
-        auth_views.PasswordResetCompleteView.as_view(),
-        name="password_reset_complete",
-    ),
+    # # Reset password urls
+    # path(
+    #     "password-reset/",
+    #     auth_views.PasswordResetView.as_view(),
+    #     name="password_reset",
+    # ),
+    # path(
+    #     "password-reset/done/",
+    #     auth_views.PasswordResetDoneView.as_view(),
+    #     name="password_reset_done",
+    # ),
+    # path(
+    #     "password-reset/<uidb64>/<token>/",
+    #     auth_views.PasswordResetConfirmView.as_view(),
+    #     name="password_reset_confirm",
+    # ),
+    # path(
+    #     "password-rest/complete/",
+    #     auth_views.PasswordResetCompleteView.as_view(),
+    #     name="password_reset_complete",
+    # ),
 
-    path("dashboard/", views.dashboard, name="dashboard"),
+    path("", include('django.contrib.auth.urls')),
+    path("", views.dashboard, name="dashboard"),
 
 ]
