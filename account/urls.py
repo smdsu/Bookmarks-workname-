4188208982cc83a96f6ef78e4513f199974dfefa
smdsu <109036946+smdsu@users.noreply.github.com>
2025-03-16 18:57:47 +0300

    Add Django base password reset

--- account/urls.py
+++ account/urls.py
@@ -8,21 +8,44 @@ from . import views
 urlpatterns = [
     # path('login/', views.user_login, name='login'),
 
-    #Login/logout urls
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # Login/logout urls
+    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 
-    #Change password urls
+    # Change password urls
     path(
-        'password-change/',
+        "password-change/",
         auth_views.PasswordChangeView.as_view(),
-        name='password_change'
+        name="password_change",
     ),
     path(
-        'password-change/done/',
+        "password-change/done/",
         auth_views.PasswordChangeDoneView.as_view(),
-        name='password_change_done'
+        name="password_change_done",
     ),
 
-    path('dashboard/', views.dashboard, name='dashboard'),
+    # Reset password urls
+    path(
+        "password-reset/",
+        auth_views.PasswordResetView.as_view(),
+        name="password_reset",
+    ),
+    path(
+        "password-reset/done/",
+        auth_views.PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path(
+        "password-reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path(
+        "password-rest/complete/",
+        auth_views.PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
+
+    path("dashboard/", views.dashboard, name="dashboard"),
+
 ]
