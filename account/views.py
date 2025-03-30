4dec3b8ef4f489711289c38642a72857267c1098
smdsu <109036946+smdsu@users.noreply.github.com>
2025-03-30 18:12:09 +0300

    Add messages

--- account/views.py
+++ account/views.py
@@ -1,5 +1,6 @@
 # from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 from django.http import HttpResponse
 from django.shortcuts import render
@@ -91,6 +92,15 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(
+                request,
+                'Profile updated successfully.'
+            )
+        else:
+            messages.error(
+                request,
+                'Error updating your profile.'
+            )
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
